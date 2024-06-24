eeaef4eb80f153791537e1c67499275007755367
Oliver Buckley <89339167+olibucko@users.noreply.github.com>
2024-06-24 18:26:22 +1000

    Initial Commit
    Added basic gesture recognition funcitonality

---
+++ app.py
@@ -0,0 +1,16 @@
+import gesture_recognition
+
+# Gesture recognition effective.
+# Facial recognition 
+
+
+
+def app():
+  print("Provide the authentication gesture")
+  gesture_recognition.main()
+  print("Gesture authenticated")
+  print("Provide facial recognition")
+
+
+
+app()
